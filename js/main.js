3f5a33a3ca72d49ab3e98ab046e77fd8076f2e9f
Maxliljeqvist <lima24nb@student.ju.se>
2025-02-21 14:12:21 +0100

    Forgot
    Forgot to add the localStorage.text, also removed the "..." from thankUElement.

--- js/main.js
+++ js/main.js
@@ -7,11 +7,12 @@ if (localStorage.text) {
 
 thankUElement.innerText = localStorage.text;
 }else {
-  thankUElement.innerText = "...";
+  thankUElement.innerText = "";
 }
 buttonElement.addEventListener("click", function (e) {
   e.preventDefault(); 
   let text = lnameElement.value;
   let text2 = fnameElement.value;
   thankUElement.innerText = `Thank you ${text2} ${text} for your submission!`;
+    localStorage.text = thankUElement.innerText;
 });
